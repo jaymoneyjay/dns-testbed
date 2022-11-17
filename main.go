fd318196ab30c70af6f75748222811ab1445589f
Jodok Vieli <jvieli@student.ethz.ch>
2022-11-17 08:53:50 +0100

    run slow dns + unbound 1.10

--- main.go
+++ main.go
@@ -4,6 +4,15 @@ import (
 	"dns-testbed-go/lab"
 )
 
+func getImplementations() []string {
+	return []string{
+		//"bind-9.18.4",
+		"unbound-1.10.0",
+		//"unbound-1.16.0",
+		//"powerDNS-4.7.3",
+	}
+}
+
 func main() {
 	dnsTestLab := lab.New("results")
 	runSlowDNSExperiment(dnsTestLab, lab.SlowDNS_CNAME_Scrubbing)
@@ -21,17 +30,8 @@ func runSubqueryExperiment(dnsTestLab *lab.Lab, experiment *lab.VolumetricExperi
 func runSlowDNSExperiment(dnsTestLab *lab.Lab, experiment *lab.TimingExperiment) {
 	dnsTestLab.Conduct(
 		experiment,
-		lab.NewDataIterator(getImplementations(), lab.MakeRange(800, 800, 200)),
+		lab.NewDataIterator(getImplementations(), lab.MakeRange(0, 1400, 200)),
 		"a1.target.com",
 	)
 	dnsTestLab.SaveResults()
-}
-
-func getImplementations() []string {
-	return []string{
-		//"bind-9.18.4",
-		//"unbound-1.10.0",
-		//"unbound-1.16.0",
-		"powerDNS-4.7.3",
-	}
 }
