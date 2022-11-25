ff8f04407ecc837ba55fcb9956e0ee70a4e5ad25
Jodok Vieli <jvieli@student.ethz.ch>
2022-11-25 16:22:03 +0100

    run DNAME+subquery

--- main.go
+++ main.go
@@ -21,15 +21,15 @@ func main() {
 	//runVolumetricExperiment(dnsTestLab, lab.Subquery_DNAME_Scrubbing, getImplementations())
 	//runTimingExperiment(dnsTestLab, lab.SlowDNS_CNAME_Scrubbing, getImplementations())
 	//runTimingExperiment(dnsTestLab, lab.SlowDNS_DNAME_Scrubbing, getImplementations())
-	runVolumetricExperiment(dnsTestLab, lab.Chain_CNAME_Scrubbing, []string{"bind-9.18.4"})
+	runVolumetricExperiment(dnsTestLab, lab.Subquery_DNAME_Scrubbing, getImplementations())
 }
 
 func runVolumetricExperiment(dnsTestLab *lab.Lab, experiment *lab.VolumetricExperiment, implementations []string) {
 	dnsTestLab.Conduct(
 		experiment,
-		lab.NewDataIterator(implementations, lab.MakeRange(9, 18, 1)),
+		lab.NewDataIterator(implementations, lab.MakeRange(1, 10, 1)),
 	)
-	//dnsTestLab.SaveResults()
+	dnsTestLab.SaveResults()
 }
 
 func runTimingExperiment(dnsTestLab *lab.Lab, experiment *lab.TimingExperiment, implementations []string) {
